f5481e5bd9dcee8708a776b440b398c0f163ed71
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2020-09-20 23:48:09 +0900

    add toggle_state()

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -15,10 +15,17 @@ void setup() {
 void loop() {
   Serial.println(++count);
   toggle = toggle_state(toggle); //toggle LED value.
-  digitalWrite(PIN_LED, toggle); // update LED status.
-  delay(1000); // wait for 1,000 milliseconds
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
 }
 
 int toggle_state(int toggle) {
+  if (toggle==0) {
+    toggle += 1;
+    }
+  else  {
+    toggle -= 1;
+    }
+      
   return toggle;
 }
